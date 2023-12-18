0c1839e151c449fdaaf47af57efee0d939191e0c
Sophia Seely <sophiaseely@outlook.com>
2023-12-18 11:32:22 -0600

    space selection when droping tokens works.

--- ConnectFour/Services/Board StateManager.cs
+++ ConnectFour/Services/Board StateManager.cs
@@ -15,185 +15,53 @@ public class BoardStateManager
                 if (connectFourGame1[rowIndex, columnIndex] != 0)
                 {
                     //right
-                    if (columnIndex + 1 < connectFourGame1.GetLength(1))
+                    if (CheckDirection(connectFourGame1, columnIndex, rowIndex, 0, +1))
                     {
-                        if (connectFourGame1[rowIndex, columnIndex + 1] == connectFourGame1[rowIndex, columnIndex])
-                        {
-                            int variableIndex = columnIndex + 1;
-                            int inARow = 1;
-                            while (variableIndex < connectFourGame1.GetLength(1) &&
-                                   connectFourGame1[rowIndex, variableIndex] == connectFourGame1[rowIndex, columnIndex])
-                            {
-                                inARow++;
-                                variableIndex++;
-                            }
-
-                            if (inARow >= 4)
-                            {
-                                return connectFourGame1[rowIndex, columnIndex];
-                            }
-                        }
+                        return connectFourGame1[rowIndex, columnIndex];
                     }
+                    
 
                     //left
-                    if (columnIndex - 1 >= 0)
+                    if (CheckDirection(connectFourGame1, columnIndex, rowIndex, 0, -1))
                     {
-                        if (connectFourGame1[rowIndex, columnIndex - 1] != 0)
-                        {
-                            int variableIndex = columnIndex - 1;
-                            int inARow = 1;
-                            while (variableIndex >= 0 && connectFourGame1[rowIndex, variableIndex] ==
-                                   connectFourGame1[rowIndex, columnIndex])
-                            {
-                                inARow++;
-                                variableIndex--;
-                            }
-
-                            if (inARow >= 4)
-                            {
-                                return connectFourGame1[rowIndex, columnIndex];
-                            }
-                        }
+                        return connectFourGame1[rowIndex, columnIndex];
                     }
 
                     //up
-                    if (rowIndex + 1 < connectFourGame1.GetLength(0))
+                    if (CheckDirection(connectFourGame1, columnIndex, rowIndex, +1, 0))
                     {
-                        if (connectFourGame1[rowIndex + 1, columnIndex] != 0)
-                        {
-                            int variableIndex = rowIndex + 1;
-                            int inARow = 1;
-                            while (variableIndex < connectFourGame1.GetLength(0) &&
-                                   connectFourGame1[variableIndex, columnIndex] ==
-                                   connectFourGame1[rowIndex, columnIndex])
-                            {
-                                inARow++;
-                                variableIndex++;
-                            }
-
-                            if (inARow >= 4)
-                            {
-                                return connectFourGame1[rowIndex, columnIndex];
-                            }
-                        }
+                        return connectFourGame1[rowIndex, columnIndex];
                     }
 
                     //down
-                    if (rowIndex - 1 >= 0)
+                    if (CheckDirection(connectFourGame1, columnIndex, rowIndex,  -1, 0))
                     {
-                        if (connectFourGame1[rowIndex - 1, columnIndex] != 0)
-                        {
-                            int variableIndex = rowIndex - 1;
-                            int inARow = 1;
-                            while (variableIndex >= 0 && connectFourGame1[variableIndex, columnIndex] ==
-                                   connectFourGame1[rowIndex, columnIndex])
-                            {
-                                inARow++;
-                                variableIndex--;
-                            }
-
-                            if (inARow >= 4)
-                            {
-                                return connectFourGame1[rowIndex, columnIndex];
-                            }
-                        }
+                        return connectFourGame1[rowIndex, columnIndex];
                     }
 
                     //top right diagonal
-                    if (columnIndex + 1 < connectFourGame1.GetLength(1) && rowIndex + 1 < connectFourGame1.GetLength(0))
+                    if (CheckDirection(connectFourGame1, columnIndex, rowIndex, +1, +1))
                     {
-                        if (connectFourGame1[rowIndex + 1, columnIndex + 1] != 0)
-                        {
-                            int variableRowIndex = rowIndex + 1;
-                            int variableColumnIndex = columnIndex + 1;
-                            int inARow = 1;
-                            while (variableColumnIndex < connectFourGame1.GetLength(1) &&
-                                   variableRowIndex < connectFourGame1.GetLength(0) &&
-                                   connectFourGame1[variableRowIndex, variableColumnIndex] ==
-                                   connectFourGame1[rowIndex, columnIndex])
-                            {
-                                inARow++;
-                                variableRowIndex++;
-                                variableColumnIndex++;
-                            }
-
-                            if (inARow >= 4)
-                            {
-                                return connectFourGame1[rowIndex, columnIndex];
-                            }
-                        }
+                        return connectFourGame1[rowIndex, columnIndex];
                     }
+                    
 
                     //botom left diagonal
-                    if (columnIndex - 1 >= 0 && rowIndex - 1 >= 0)
+                    if (CheckDirection(connectFourGame1, columnIndex, rowIndex, -1, -1))
                     {
-                        if (connectFourGame1[rowIndex - 1, columnIndex - 1] != 0)
-                        {
-                            int variableRowIndex = rowIndex - 1;
-                            int variableColumnIndex = columnIndex - 1;
-                            int inARow = 1;
-                            while (variableRowIndex >= 0 && variableColumnIndex >= 0 &&
-                                   connectFourGame1[variableRowIndex, variableColumnIndex] ==
-                                   connectFourGame1[rowIndex, columnIndex])
-                            {
-                                inARow++;
-                                variableRowIndex--;
-                                variableColumnIndex--;
-                            }
-
-                            if (inARow >= 4)
-                            {
-                                return connectFourGame1[rowIndex, columnIndex];
-                            }
-                        }
+                        return connectFourGame1[rowIndex, columnIndex];
                     }
 
                     //top left diagonal
-                    if (columnIndex + 1 < connectFourGame1.GetLength(1) && rowIndex - 1 >= 0)
+                    if (CheckDirection(connectFourGame1, columnIndex, rowIndex, -1, +1))
                     {
-                        if (connectFourGame1[rowIndex - 1, columnIndex + 1] != 0)
-                        {
-                            int variableRowIndex = rowIndex - 1;
-                            int variableColumnIndex = columnIndex + 1;
-                            int inARow = 1;
-                            while (variableColumnIndex < connectFourGame1.GetLength(1) && variableRowIndex >= 0 &&
-                                   connectFourGame1[variableRowIndex, variableColumnIndex] ==
-                                   connectFourGame1[rowIndex, columnIndex])
-                            {
-                                inARow++;
-                                variableRowIndex--;
-                                variableColumnIndex++;
-                            }
-
-                            if (inARow >= 4)
-                            {
-                                return connectFourGame1[rowIndex, columnIndex];
-                            }
-                        }
+                        return connectFourGame1[rowIndex, columnIndex];
                     }
 
                     //bottom right diagonal
-                    if (columnIndex - 1 >= 0 && rowIndex + 1 < connectFourGame1.GetLength(0))
+                    if (CheckDirection(connectFourGame1, columnIndex, rowIndex, +1, -1))
                     {
-                        if (connectFourGame1[rowIndex + 1, columnIndex - 1] != 0)
-                        {
-                            int variableRowIndex = rowIndex + 1;
-                            int variableColumnIndex = columnIndex - 1;
-                            int inARow = 1;
-                            while (variableColumnIndex >= 0 && variableRowIndex < connectFourGame1.GetLength(0) &&
-                                   connectFourGame1[variableRowIndex, variableColumnIndex] ==
-                                   connectFourGame1[rowIndex, columnIndex])
-                            {
-                                inARow++;
-                                variableRowIndex++;
-                                variableColumnIndex--;
-                            }
-
-                            if (inARow >= 4)
-                            {
-                                return connectFourGame1[rowIndex, columnIndex];
-                            }
-                        }
+                        return connectFourGame1[rowIndex, columnIndex];
                     }
                 }
 
@@ -204,5 +72,56 @@ public class BoardStateManager
         }
 
         return Player.NoOne;
+    }
+
+    private bool CheckDirection(Player[,] connectFourGame1, int columnIndex, int rowIndex, int rowDelta, int columnDelta)
+    {
+        int variableRowIndex = rowIndex + rowDelta;
+        int variableColumnIndex = columnIndex + columnDelta;
+        if (InBounds(variableColumnIndex, variableRowIndex, connectFourGame1) && connectFourGame1[variableRowIndex, variableColumnIndex] != 0)
+        {
+           
+            int inARow = 1;
+            while (InBounds(variableColumnIndex, variableRowIndex, connectFourGame1) &&
+                   connectFourGame1[variableRowIndex, variableColumnIndex] ==
+                   connectFourGame1[rowIndex, columnIndex])
+            {
+                inARow++;
+                variableRowIndex += rowDelta;
+                variableColumnIndex += columnDelta;
+            }
+
+            if (inARow >= 4)
+            {
+                return true;
+                
+            }
+        }
+
+        return false;
+    }
+
+    public bool InBounds(int variableColumnIndex, int variableRowIndex, Player[,] connectFourGame1)
+    {
+        if (variableColumnIndex < 0 || variableRowIndex >= connectFourGame1.GetLength(0) ||
+            variableColumnIndex >= connectFourGame1.GetLength(1) || variableRowIndex < 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void PlaceToken(Player[,] connectFourGame, int whichColumn, Player whichPlayer)
+    {
+        int row = 0;
+        while ( row < connectFourGame.GetLength(0) && connectFourGame[row, whichColumn] == Player.NoOne )
+        {
+            row++;
+            
+        }
+
+        connectFourGame[row-1, whichColumn] = whichPlayer;
+
     }
 }
--- ConnectFourTest/UnitTest1.cs
+++ ConnectFourTest/UnitTest1.cs
@@ -90,7 +90,7 @@ public class WhenGivenA2DArrayWithPlayerOneHavingFourDown
     }
 }
 
-public class WhenGivenA2DArrayAndPlayerOneHasFourDiagonal
+public class WhenGivenA2DArrayAndPlayerOneHasFourDiagonalDown
 {
     Player [,] connectFourGame3 = new Player [6, 7]
     {
@@ -119,3 +119,127 @@ public class WhenGivenA2DArrayAndPlayerOneHasFourDiagonal
     }
 }
 //write a diagonal test going the other way with player two winning.
+public class WhenGivenA2DArrayAndPlayerOneHasFourDiagonalUp
+{
+    Player [,] connectFourGame3 = new Player [6, 7]
+    {
+        { 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, (Player) 1, (Player)2, 0, 0 },
+        { 0, 0, 0,(Player) 2, 0, 0, 0 },
+        { 0, 0,(Player) 2, 0, 0, 0, 0 },
+        { 0,(Player) 2, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0 },
+    };
+    [SetUp]
+    public void Setup()
+    {
+ 
+        
+
+    }
+
+    [Test]
+    public void ThenItCanFindFourDiagonal()
+    {
+        BoardStateManager boardStateManager = new BoardStateManager();
+        Player actual = boardStateManager.FourInARow(connectFourGame3);
+        Player expected = Player.PlayerTwo;
+        Assert.AreEqual(expected, actual);
+    }
+
+    public class WhenGivenAnEmpty2DArrayAPlayerAndAColumn
+    {
+        Player [,] connectFourGame4 = new Player [6, 7]
+        {
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+        };
+
+        int column = 3;
+        private Player player = Player.PlayerOne;
+
+        [SetUp]
+        public void Setup()
+        {
+ 
+            BoardStateManager boardStateManager = new BoardStateManager();
+            boardStateManager.PlaceToken(connectFourGame4, column, player);
+        }
+        [Test]
+        public void ThenItCanFindTheNextOpenSpace()
+        {
+            Player expected = Player.PlayerOne;
+            Player actual = connectFourGame4[5, 3];
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class WhenGivenAPartialyPopulated2DArrayAPlayerAndAColumn
+    {
+        Player [,] connectFourGame5 = new Player [6, 7]
+        {
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, (Player) 1, 0, 0, 0 },
+            { 0, 0, 0, (Player) 1, 0, 0, 0 },
+        };
+
+        int column = 3;
+       Player player = Player.PlayerOne;
+       
+       [SetUp]
+       public void Setup()
+       {
+ 
+           BoardStateManager boardStateManager = new BoardStateManager();
+           boardStateManager.PlaceToken(connectFourGame5, column, player);
+       }
+       
+       [Test]
+       public void ThenItCanFindTheNextOpenSpace()
+       {
+           Player expected = Player.PlayerOne;
+           Player actual = connectFourGame5[3, 3];
+           Assert.AreEqual(expected, actual);
+       }
+
+    }
+
+    public class WhenGivenAPartiallyPopulated2DArrayAPlayerAndAColumn
+    {
+        Player[,] connectFourGame6 = new Player [6, 7]
+        {
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, (Player) 1, 0, 0, 0, 0 },
+            { 0, 0, (Player) 1, 0, 0, 0, 0 },
+        };
+
+        int column = 2;
+        private Player player = Player.PlayerTwo;
+        
+        [SetUp]
+        public void Setup()
+        {
+ 
+            BoardStateManager boardStateManager = new BoardStateManager();
+            boardStateManager.PlaceToken(connectFourGame6, column, player);
+        }
+        
+        [Test]
+        public void ThenItCanFindTheNextOpenSpace()
+        {
+            Player expected = Player.PlayerTwo;
+            Player actual = connectFourGame6[3, 2];
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
